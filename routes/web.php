6965b93886e7fa2bec955b6952e28dcffbf8213a
Ashu saini <ashusaini514@gmail.com>
2022-01-31 23:21:44 +0530

    Created login,logout,register,forgot password , reset password views and controller functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('register', [RegisterController::class, 'register']);
+Route::post('register', [RegisterController::class, 'store'])->name('register');
+Route::get('login', [LoginController::class, 'login']);
+Route::post('login', [LoginController::class, 'store'])->name('login');
+Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('home', [LoginController::class, 'home'])->name('home');
+Route::get('forget-password', [ForgotPasswordController::class, 'getEmail']);
+Route::post('forget-password', [ForgotPasswordController::class, 'postEmail']);
+Route::get('reset-password/{token}', [ResetPasswordController::class, 'getPassword']);
+Route::post('reset-password', [ResetPasswordController::class, 'updatePassword']);
